a82c72b92b2495860a549ebde04e7228dc46a61b
Brice Sanchez <contact@brice-sanchez.com>
2016-08-22 23:44:02 -0400

    Comment Spree seeds
    Comment Spree seeds because Spree load seeds only on database creation
    while Refinery CMS loads seeds on each extension installation with the
    command rake db:seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,5 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-Spree::Core::Engine.load_seed if defined?(Spree::Core)
-Spree::Auth::Engine.load_seed if defined?(Spree::Auth)
+# Spree::Core::Engine.load_seed if defined?(Spree::Core)
+# Spree::Auth::Engine.load_seed if defined?(Spree::Auth)
